d5aacab2ec449b84058807d1bee0cbf05e806d05
Maarten Thiebou <thiebou@gmail.com>
2022-02-19 00:03:02 +0100

    curly brace access syntax is deprecated since php 7.4

--- app/libraries/Profiler.php
+++ app/libraries/Profiler.php
@@ -68,7 +68,7 @@ class CI_Profiler {
 		{
 			if ( ! isset($config[$section]))
 			{
-				$this->_compile_{$section} = TRUE;
+				$this->_compile_[$section] = TRUE;
 			}
 		}
 
@@ -91,7 +91,7 @@ class CI_Profiler {
 		{
 			if (in_array($method, $this->_available_sections))
 			{
-				$this->_compile_{$method} = ($enable !== FALSE) ? TRUE : FALSE;
+				$this->_compile_[$method] = ($enable !== FALSE) ? TRUE : FALSE;
 			}
 		}
 	}
@@ -534,7 +534,7 @@ class CI_Profiler {
 
 		foreach ($this->_available_sections as $section)
 		{
-			if ($this->_compile_{$section} !== FALSE)
+			if ($this->_compile_[$section] !== FALSE)
 			{
 				$func = "_compile_{$section}";
 				$output .= $this->{$func}();
