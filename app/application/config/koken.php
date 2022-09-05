4e2cf71a10b6b2129e4f54898aa7f1e02241648d
Maarten Thiebou <thiebou@gmail.com>
2022-09-05 23:53:24 +0200

    version bump 1.1.4

--- app/application/config/koken.php
+++ app/application/config/koken.php
@@ -48,7 +48,7 @@ if (!defined('AUTO_UPDATE')) {
 }
 
 // Director constants
-define('KOKEN_VERSION', '0.22.24');
+define('KOKEN_VERSION', '1.1.4');
 
 /* End of file koken.php */
 /* Location: ./system/application/config/koken.php */
