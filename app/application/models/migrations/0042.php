b1e363699ed3c49fcd88e85b549f32b700ba6eb6
Maarten Thiebou <thiebou@gmail.com>
2022-10-06 00:11:58 +0200

    combine last two migrations

--- app/application/models/migrations/0042.php
+++ app/application/models/migrations/0042.php
@@ -2,6 +2,6 @@
 
 $d = new Draft();
 
-$this->db->query("ALTER TABLE {$d->table} MODIFY live_data MEDIUMTEXT, MODIFY data MEDIUMTEXT");
+$this->db->query("ALTER TABLE {$d->table} MODIFY live_data MEDIUMTEXT, MODIFY data MEDIUMTEXT NULL");
 
 $done = true;
--- app/application/models/migrations/0043.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$d = new Draft();
-
-$this->db->query("ALTER TABLE {$d->table} MODIFY live_data MEDIUMTEXT NULL");
-
-$done = true;
