48e2b32fb23e8ea9d7b0ee6695c99415eb6bc98f
Maarten Thiebou <thiebou@gmail.com>
2023-03-10 21:07:15 +0100

    Version Bump

--- app/site/site.php
+++ app/site/site.php
@@ -1,7 +1,7 @@
 <?php
 
     error_reporting(0);
-    define('KOKEN_VERSION', '1.1.6');
+    define('KOKEN_VERSION', '1.1.7');
     define('BASEPATH', true);
 
     ini_set('default_charset', 'UTF-8');
